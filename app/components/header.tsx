6f3e7947618866bd78411cd5973c0d053c6e4f9f
Eugene Heynike <eugeneheynike@nexablend.co.za>
2024-08-18 14:51:28 +0200

    navbar looking nice and is responsive

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,11 +4,31 @@ import Image from 'next/image';
 
 const Header: React.FC = () => {
   return (
-    <header className="w-full bg-[#fff5c4] py-4 shadow-sm sticky top-0 left-0 right-0">
-      <div className="container mx-auto flex items-center justify-between px-4 md:px-6">
-        <div className="flex items-center space-x-4">
+    <header className="w-full bg-[#fff5c4] py-4 shadow-sm sticky top-0 left-0 right-0 md:h-[130px]">
+      <div className="container mx-auto flex items-center justify-between px-4 md:px-3 lg:px-4 lg:pt-4 xl:pt-0">
+        <div className="flex items-center space-x-4 md:justify-start">
           <Link href="/">
-            <Image src="/GENY1.png" alt="Logo" width={384} height={128} />
+            <Image 
+              src="/GENY1.png" 
+              alt="Logo" 
+              width={384} 
+              height={128} 
+              className="hidden xl:block" 
+            />
+            <Image 
+              src="/GENY3.png" 
+              alt="Logo" 
+              width={241} 
+              height={81} 
+              className="hidden lg:block xl:hidden" 
+            />
+            <Image 
+              src="/logo_no_bg.png" 
+              alt="Logo" 
+              width={400} 
+              height={400} 
+              className="hidden md:block lg:hidden" 
+            />
           </Link>
         </div>
       </div>
